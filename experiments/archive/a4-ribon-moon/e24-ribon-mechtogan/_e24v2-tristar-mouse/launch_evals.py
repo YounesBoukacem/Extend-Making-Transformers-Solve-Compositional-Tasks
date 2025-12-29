e325431dcd9b46a3b1b9935975fe35e2892b0b62
Younes Boukacem <yo.boukacem@gmail.com>
2025-12-30 03:23:23 +0400

    commit

---
+++ experiments/archive/a4-ribon-moon/e24-ribon-mechtogan/_e24v2-tristar-mouse/launch_evals.py
@@ -0,0 +1,31 @@
+# Create a subfolder for each eval i.e. for each test set
+# Launch the eval script in each subfolder
+import os
+import shutil
+from pathlib import Path
+
+TESTS_ROOT_FOLDER = "/data/yb2618/DL_project_MVA/data/d11-majin-bench/"
+DDIR = "/data/yb2618/DL_project_MVA/data/d7-push-majin/"
+
+test_sets = [
+	"ood_nb_inputs.txt",
+	"ood_str_length_only_one.txt",
+	"ood_str_length_all.txt",
+	"ood_nb_inputs_str_length_only_one.txt",
+	"ood_nb_inputs_str_length_all.txt"	
+]
+
+for deviceid, test_set in enumerate(test_sets):
+	
+	# Create the eval subfolder
+	eval_name = test_set.replace(".txt", "")
+	eval_folder = Path(f"./{eval_name}")
+	eval_folder.mkdir(parents=True, exist_ok=True)
+	
+	# Copy the eval script into the eval folder
+	shutil.copyfile("./eval.py", eval_folder / "eval.py")
+	shutil.copyfile("./tokenizer.py", eval_folder / "tokenizer.py")
+	
+	# Launch the script in the eval folder
+	# Make sure the python instance is launched from within the eval folder (i.e. the cwd is the eval folder)
+	os.system(f"cd {eval_folder} && python eval.py --test_set_path {TESTS_ROOT_FOLDER}{test_set} --ddir {DDIR} --deviceid {deviceid}")
